150b799fc003fea3427818fab8bf3ba1d653c562
Mihail <schutow.mihail@yandex.ru>
2022-05-29 18:58:00 +0300

    h1 completed

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import styles from './Message.module.css'
 
-function Message() {
+
+export type MessageType = {
+    avatar: string;
+    name: string;
+    message: string;
+    time: string;
+}
+
+function Message(props: MessageType) {
     return (
-        <div>
-
+        <div className={styles.message}>
+            <img className={styles.message_img} src={props.avatar} alt=""/>
+            <div className={styles.message_block}>
+                <div className={styles.message_name}>{props.name}</div>
+                <div>{props.message}</div>
+                <div className={styles.message_time}>{props.time}</div>
+            </div>
         </div>
     )
 }
