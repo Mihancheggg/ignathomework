cbb404bc5a592fa6c53d0f7056c77c78ee6e3123
Mihail Schutow <schutow.mihail@yandex.ru>
2022-07-25 18:39:56 +0300

    Task 8 done

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,38 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+import {UserType} from '../HW8';
+
+type ActionsType = SortActionType | CheckActionType
+
+type SortActionType = {
+    type: 'sort'
+    payload: 'up' | 'down'
+}
+
+type CheckActionType = {
+    type: 'check'
+    payload: number
+}
+
+export const homeWorkReducer = (state: Array<UserType>, action: ActionsType): Array<UserType> => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // need to fix
-            return state
+            if (action.payload === 'up') {
+                let newState = [...state]
+                return newState.sort((a, b) => {
+                    return a.name.localeCompare(b.name)
+                })
+            } else if (action.payload === 'down') {
+                let newState = [...state]
+                return newState.sort((a, b) => {
+                    return b.name.localeCompare(a.name)
+                })
+            } else {
+                return state
+            }
         }
         case 'check': {
-            // need to fix
-            return state
+            return state.filter(el => el.age >= action.payload)
         }
-        default: return state
+        default:
+            return state
     }
 }
